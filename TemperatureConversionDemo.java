487a1c438f4e2c9d40dd9599e2f8e2b154fd6fbb
muhammad rofi <muhammadrofi062@gmail.com>
2023-12-31 20:08:04 +0700

    add fitur exit & looping

--- TemperatureConversionDemo.java
+++ TemperatureConversionDemo.java
@@ -1,4 +1,3 @@
-
 // Import untuk mengakses BufferedReader dan InputStreamReader untuk input.
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -9,53 +8,62 @@ public class TemperatureConversionDemo {
         // Membuat objek TemperatureBuilder.
         TemperatureBuilder temperatureBuilder = new TemperatureBuilder();
 
-        try {
-            // Membuat objek BufferedReader untuk membaca input dari pengguna.
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean exit = false;
 
-            // Menampilkan pilihan konversi suhu kepada pengguna.
-            System.out.println(
-                    "Pilih konversi suhu yang akan dipakai: \n\n1. Celcius to Fahrenheit & Kelvin\n2. Fahrenheit to Celcius & Kelvin\n3. Kelvin to Celcius & Fahrenheit\n\nEnter Number : ");
+        while (!exit) {
+            try {
+                // Membuat objek BufferedReader untuk membaca input dari pengguna.
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            // Membaca input pengguna.
-            Integer userInput = Integer.parseInt(br.readLine());
+                System.out.println("----------------------------------------");
 
-            // Build dan menampilkan suhu sesuai input dari pengguna.
-            if (userInput == 1) {
-                // Build dan menampilkan suhu dalam Celsius beserta konversinya.
-                TemperatureType suhuCelsius = temperatureBuilder.buildCelsiusTemperature();
-                System.out.println("\nSuhu dalam Celsius: " + suhuCelsius.getItems().get(0).getTemperature());
+                // Menampilkan pilihan konversi suhu kepada pengguna.
                 System.out.println(
-                        "Suhu dalam Fahrenheit: "
-                                + ((CelsiusTemperature) suhuCelsius.getItems().get(0)).convertToFahrenheit());
-                System.out.println(
-                        "Suhu dalam Kelvin: " + ((CelsiusTemperature) suhuCelsius.getItems().get(0)).convertToKelvin());
-            } else if (userInput == 2) {
-                // Build dan menampilkan suhu dalam Fahrenheit beserta konversinya.
-                TemperatureType suhuFahrenheit = temperatureBuilder.buildFahrenheitTemperature();
-                System.out.println("\nSuhu dalam Fahrenheit: " + suhuFahrenheit.getItems().get(0).getTemperature());
-                System.out.println(
-                        "Suhu dalam Celsius: "
-                                + ((FahrenheitTemperature) suhuFahrenheit.getItems().get(0)).convertToCelsius());
-                System.out.println(
-                        "Suhu dalam Kelvin: "
-                                + ((FahrenheitTemperature) suhuFahrenheit.getItems().get(0)).convertToKelvin());
-            } else if (userInput == 3) {
-                // Build dan menampilkan suhu dalam Kelvin beserta konversinya.
-                TemperatureType suhuKelvin = temperatureBuilder.buildKelvinTemperature();
-                System.out.println("\nSuhu dalam Kelvin: " + suhuKelvin.getItems().get(0).getTemperature());
-                System.out.println(
-                        "Suhu dalam Celsius: " + ((KelvinTemperature) suhuKelvin.getItems().get(0)).convertToCelsius());
-                System.out.println(
-                        "Suhu dalam Fahrenheit: "
-                                + ((KelvinTemperature) suhuKelvin.getItems().get(0)).convertToFahrenheit());
-            } else {
-                // Menangani apabila pengguna mengInputkan angka yang tidak sesuai.
-                System.out.println("Invalid Input");
+                        "\nPilih konversi suhu yang akan dipakai: \n\n1. Celcius to Fahrenheit & Kelvin\n2. Fahrenheit to Celcius & Kelvin\n3. Kelvin to Celcius & Fahrenheit\n4. Exit\n");
+                System.out.print("Pilih Nomor: ");
+                // Membaca input pengguna.
+                Integer userInput = Integer.parseInt(br.readLine());
+
+                // Build dan menampilkan suhu sesuai input dari pengguna.
+                if (userInput == 1) {
+                    // Build dan menampilkan suhu dalam Celsius beserta konversinya.
+                    TemperatureType suhuCelsius = temperatureBuilder.buildCelsiusTemperature();
+                    System.out.println("\nSuhu dalam Celsius: " + suhuCelsius.getItems().get(0).getTemperature());
+                    System.out.println(
+                            "Suhu dalam Fahrenheit: "
+                                    + ((CelsiusTemperature) suhuCelsius.getItems().get(0)).convertToFahrenheit());
+                    System.out.println(
+                            "Suhu dalam Kelvin: " + ((CelsiusTemperature) suhuCelsius.getItems().get(0)).convertToKelvin());
+                } else if (userInput == 2) {
+                    // Build dan menampilkan suhu dalam Fahrenheit beserta konversinya.
+                    TemperatureType suhuFahrenheit = temperatureBuilder.buildFahrenheitTemperature();
+                    System.out.println("\nSuhu dalam Fahrenheit: " + suhuFahrenheit.getItems().get(0).getTemperature());
+                    System.out.println(
+                            "Suhu dalam Celsius: "
+                                    + ((FahrenheitTemperature) suhuFahrenheit.getItems().get(0)).convertToCelsius());
+                    System.out.println(
+                            "Suhu dalam Kelvin: "
+                                    + ((FahrenheitTemperature) suhuFahrenheit.getItems().get(0)).convertToKelvin());
+                } else if (userInput == 3) {
+                    // Build dan menampilkan suhu dalam Kelvin beserta konversinya.
+                    TemperatureType suhuKelvin = temperatureBuilder.buildKelvinTemperature();
+                    System.out.println("\nSuhu dalam Kelvin: " + suhuKelvin.getItems().get(0).getTemperature());
+                    System.out.println(
+                            "Suhu dalam Celsius: " + ((KelvinTemperature) suhuKelvin.getItems().get(0)).convertToCelsius());
+                    System.out.println(
+                            "Suhu dalam Fahrenheit: "
+                                    + ((KelvinTemperature) suhuKelvin.getItems().get(0)).convertToFahrenheit());
+                } else if (userInput == 4) {
+                    // Exit dari program.
+                    exit = true;
+                } else {
+                    // Menangani apabila pengguna mengInputkan angka yang tidak sesuai.
+                    System.out.println("Invalid Input");
+                }
+            } catch (Exception e) {
+                // Mencetak Stack Trace.
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            // Mencetak Stack Trace.
-            e.printStackTrace();
         }
     }
 }
